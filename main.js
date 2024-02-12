c4dd4214d7b78167c2b86c617c7b427fd975d61a
Chris <hereokay88@gmail.com>
2024-02-13 01:04:08 +0900

    refactor: extract MessageObj 함수생성

--- main.js
+++ main.js
@@ -19,6 +19,71 @@ function checkAndBlock(content, userName) {
   }
 }
 
+
+
+function extractMessageToDB(messageObj){
+  try {
+    // MESSAGE_CREATE 유형의 메시지인지 확인
+    if (messageObj.t === 'MESSAGE_CREATE') {
+      // console.log(messageObj)
+      // 필요한 정보 추출
+      
+      let globalName = messageObj.d.author.global_name; // 채널별명
+      const userName = messageObj.d.author.username; // 진짜이름
+      const guildId = messageObj.d.guild_id // 메이플랜드 채널
+      const channelId = messageObj.d.channel_id; // 경매장 or 파티
+      const nick = messageObj.d.member.nick;
+      const msgId = messageObj.d.id;
+      
+      if(nick !== null ){
+        // nick이 있으먄 우선순위
+        globalName = nick;
+      }
+      else{
+        // 닉이 없고 globalName이 있으면
+        if (globalName !== null){
+          globalName = globalName;
+        }
+        else{
+          //다 없으면
+          globalName = userName;
+        }
+      }
+
+      if (globalName===null){
+        if (nick !== null){
+          globalName = nick;
+        }
+        else{
+          globalName = userName;
+        }
+      }
+
+      const timeStamp =  messageObj.d.timestamp;
+      let content = messageObj.d.content.replace(/<[^>]*>/g, "");
+      // 공백을 기준으로 단어 분리, 중복 제거 후 다시 결합
+      content = [...new Set(content.split(/\s+/))].join(' ');
+      
+      checkAndBlock(content, userName);
+
+      // 메이플랜드 채널
+      if(guildId !== '1134059900666916935'){
+        return;
+      }
+      
+     res = saveChatToDB(globalName,userName,content,guildId,channelId,msgId,timeStamp);
+     
+    }
+  } catch (error) {
+    console.error('Error extractMessageToDB :', error);
+  }
+}
+
+let messageBuffer = [];
+let tempBuffer = []; // Temporary buffer to hold messages during DB operations
+const savingLock = { isSaving: false }; // Lock to track DB save operation
+
+
 onMessage(function(message) {
   if (message instanceof Buffer) {
     const messageString = message.toString('utf-8');
@@ -31,53 +96,17 @@ onMessage(function(message) {
         // console.log(messageObj)
         // 필요한 정보 추출
         
-        let globalName = messageObj.d.author.global_name; // 채널별명
-        const userName = messageObj.d.author.username; // 진짜이름
-        const guildId = messageObj.d.guild_id // 메이플랜드 채널
-        const channelId = messageObj.d.channel_id; // 경매장 or 파티
-        const nick = messageObj.d.member.nick;
-        const msgId = messageObj.d.id;
-        
-        if(nick !== null ){
-          // nick이 있으먄 우선순위
-          globalName = nick;
-        }
-        else{
-          // 닉이 없고 globalName이 있으면
-          if (globalName !== null){
-            globalName = globalName;
-          }
-          else{
-            //다 없으면
-            globalName = userName;
-          }
-        }
-
-        if (globalName===null){
-          if (nick !== null){
-            globalName = nick;
-          }
-          else{
-            globalName = userName;
-          }
-        }
-
-        const timeStamp =  messageObj.d.timestamp;
-        let content = messageObj.d.content.replace(/<[^>]*>/g, "");
-        // 공백을 기준으로 단어 분리, 중복 제거 후 다시 결합
-        content = [...new Set(content.split(/\s+/))].join(' ');
-        
-        checkAndBlock(content, userName);
-
-        // 메이플랜드 채널
-        if(guildId !== '1134059900666916935'){
-          return;
-        }
-        
-       res = saveChatToDB(globalName,userName,content,guildId,channelId,msgId,timeStamp);
+        extractMessageToDB(messageObj);
       }
     } catch (error) {
       console.error('Error parsing message:', error);
     }
   }
 });
+
+function saveBufferToDB(){
+  savingLock.isSaving = true; // Lock to prevent new saves
+
+
+  messageBuffer
+}
